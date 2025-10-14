4a8698409bc7ce49d75e26263dcbd46d1dfbb00d
j0ttaape <joaopedrotrajano59@gmail.com>
2025-10-14 18:42:33 -0300

    indo pro ex2

--- Program.cs
+++ Program.cs
@@ -1,59 +1,15 @@
-﻿namespace atividade_sessao_logica_part2_c_;
-
-class Program
+﻿namespace atividade_sessao_logica_part2_c_
 {
-    static void Main(string[] args)
+    class Program
     {
-        Apresentar();
-        
-        Pedir();
+        static void Main(string[] args)
+        {
 
-        Quantidade();
-        Calculo(tipo, qtd);
-
-        Console.WriteLine($"O total a se pagar é {Calculo}");
+        }
     }
 
     public static void Apresentar()
-    {
-        Console.WriteLine("------------ PROGRAMA DO INGRESSO DO CINEMA ------------");
-
-    }
-
-    public static string Pedir ()
-    {
-        Console.WriteLine("Me informe qual tipo de ingresso você vai utilizar");
-        string tipo = Convert.ToString(Console.ReadLine());
-        return tipo;
-
-    }
-
-    public static int Quantidade ()
-    {
-        Console.WriteLine("Me informe a quantidade");
-        int qtd = Convert.ToInt16(Console.ReadLine());
-        return qtd;
-
-    }
-
-    public static double Calculo (string tipo, int qtd)
-    {
-        double resultado = 0;
-        double meia = 14.25;
-        
-        double inteira = 28.5;
-        
-        if(tipo == "meia")
         {
-            resultado = meia*qtd;
+            Console.WriteLine("")
         }
-
-        else if(tipo =="inteira")
-        {
-            resultado = inteira*qtd;
-        }
-
-        return resultado;
-
-    }
 }
